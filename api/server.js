44f1b0e2a82529fef58da57d1b288af30c898c1c
inamkhn <inamkhn7786@gmail.com>
2024-01-27 08:16:43 -0800

    pre build installation

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import path from 'path';
 
 import userRouter from './Routers/userRoute.js'
 import authRouter from './Routers/authRoute.js'
@@ -20,13 +21,28 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.send("hello")
-})
+const __dirname = path.resolve();
 
 app.use("/api/v1",userRouter)
 app.use("/api/v1",authRouter)
 app.use("/api/v1",listingRouter)
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(5000,(req,res)=>{
     connect()
